ab8a435c400d0db451bdf528d9d4e2c68bb64c83
Samuel Pritchard <samuel.pritchard@digital.cabinet-office.gov.uk>
2020-04-21 10:04:30 +0100

    Add base url logic after revert

--- nessus.py
+++ nessus.py
@@ -8,15 +8,15 @@ import boto3
 from cloudwatch import send_logs_to_cloudwatch as send_to_cloudwatch
 
 
-def get_keys_from_ssm(key):
+def get_param_from_ssm(param):
     ssm_client = boto3.client("ssm")
-    response = ssm_client.get_parameter(Name=f"/nessus/{key}_key", WithDecryption=True)
+    response = ssm_client.get_parameter(Name=f"/nessus/{param}", WithDecryption=True)
     return response["Parameter"]["Value"]
 
 
 def create_custom_headers():
-    access_key = os.getenv("access_key", get_keys_from_ssm("access"))
-    secret_key = os.getenv("secret_key", get_keys_from_ssm("secret"))
+    access_key = os.getenv("access_key", get_param_from_ssm("access_key"))
+    secret_key = os.getenv("secret_key", get_param_from_ssm("secret_key"))
     if access_key:
         return {"X-ApiKeys": f"accessKey={access_key}; secretKey={secret_key}"}
     else:
@@ -51,8 +51,7 @@ def process_csv(csv_text):
 
 
 def main(event, context):
-    nessus_ip = os.getenv("nessus_ip")
-    base_url = f"https://{nessus_ip}:8834"
+    base_url = os.getenv("base_url", get_param_from_ssm("base_url"))
     custom_headers = create_custom_headers()
     token = prepare_export(custom_headers, base_url)
     csv_text = token_download(token, base_url, custom_headers)
