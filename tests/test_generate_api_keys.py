5686488d912ffe10e16086bc06df15f9733a2502
Samuel Pritchard <samuel.pritchard@digital.cabinet-office.gov.uk>
2020-05-04 14:34:15 +0100

    Test get ec2 param

---
+++ tests/test_generate_api_keys.py
@@ -0,0 +1,26 @@
+import sys
+import os
+
+import vcr
+
+currentdir = os.path.dirname(__file__)
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
+import generate_api_keys as gak
+
+
+my_vcr = vcr.VCR(
+    record_mode="none",
+    match_on=["uri", "method", "body"],
+    cassette_library_dir="tests/fixtures/cassettes",
+    path_transformer=vcr.VCR.ensure_suffix(".yaml"),
+)
+
+
+@my_vcr.use_cassette(record_mode="once")
+def test_get_ec2_param():
+    param = "PublicIpAddress"
+    result = gak.get_ec2_param(param)
+    expected = "127.0.0.1"
+    assert result == expected
