c3a9d58b8bffd4c2e58b2e8b952085cbd36ba976
Samuel Pritchard <samuel.pritchard@digital.cabinet-office.gov.uk>
2020-05-04 12:56:31 +0100

    Test find scans

--- tests/test_process_scans.py
+++ tests/test_process_scans.py
@@ -1,6 +1,16 @@
 from unittest.mock import call
 import process_scans as ps
 
+import vcr
+
+
+my_vcr = vcr.VCR(
+    record_mode="none",
+    match_on=["uri", "method", "body"],
+    cassette_library_dir="tests/fixtures/cassettes",
+    path_transformer=vcr.VCR.ensure_suffix(".yaml"),
+)
+
 
 def test_process_csv(mocker):
     """The process csv function should forward each csv row
@@ -20,3 +30,10 @@ def test_process_csv(mocker):
     expected = [call('1,1,1,"1\n1"\n2,2,2,"2\n2"\n')]
 
     assert ps.process_csv.call_args_list == expected
+
+
+@my_vcr.use_cassette()
+def test_find_scans(capsys):
+    ps.find_scans()
+    captured = capsys.readouterr()
+    assert captured.out == "Scan localhost has not run.\n"
