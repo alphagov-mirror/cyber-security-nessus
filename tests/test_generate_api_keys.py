c3082de770626381e9266b41e758720d87a67251
Samuel Pritchard <samuel.pritchard@digital.cabinet-office.gov.uk>
2020-05-05 09:26:16 +0100

    Fix errors

--- tests/test_generate_api_keys.py
+++ tests/test_generate_api_keys.py
@@ -1,7 +1,6 @@
 import sys
 import os
 
-import pytest
 import vcr
 
 currentdir = os.path.dirname(__file__)
@@ -25,14 +24,14 @@ def test_get_status_checks():
     expected = True
     assert result == expected
 
-@my_vcr.use_cassette()
+@vcr.use_cassette()
 def test_get_public_url():
     result = gak.get_public_url()
     expected = "https://127.0.0.1:8834"
     assert result == expected
 
 
-@my_vcr.use_cassette()
+@vcr.use_cassette()
 def test_get_private_url():
     result = gak.get_private_url()
     expected = "https://10.0.0.0:8834"
@@ -40,7 +39,7 @@ def test_get_private_url():
 
 
 def test_put_keys():
-    with my_vcr.use_cassette("tests/fixtures/cassettes/test_put_keys.yaml") as cass:
+    with vcr.use_cassette("tests/fixtures/cassettes/test_put_keys.yaml") as cass:
         gak.put_keys()
         response = '{"Tier":"Standard","Version":25}'
         assert cass.responses[9]["body"]["string"].decode("utf-8") == response
@@ -50,10 +49,9 @@ def test_put_keys():
 
 
 def test_put_param():
-    with my_vcr.use_cassette("tests/fixtures/cassettes/test_put_param.yaml") as cass:
+    with vcr.use_cassette("tests/fixtures/cassettes/test_put_param.yaml") as cass:
         access_key = "ACCESS_KEY"
         gak.put_param(access_key, type="access_key")
         response = '{"Tier":"Standard","Version":25}'
         assert cass.responses[0]["body"]["string"].decode("utf-8") == response
         assert cass.responses[0]["status"]["code"] == 200
->>>>>>> c2b069703313b2b3dc3402ec3a013724240fa893
