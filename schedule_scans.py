01272059c89a0ba4cf508af59575efbbd672b67d
Samuel Pritchard <samuel.pritchard@digital.cabinet-office.gov.uk>
2020-05-05 17:41:47 +0100

    Check for existing scans and update instead of creating identical scan or leave if no changes

--- schedule_scans.py
+++ schedule_scans.py
@@ -10,6 +10,7 @@ from nessus import (
     create_scan,
     create_policy,
     policy_details,
+    list_scans
 )
 
 
@@ -69,15 +70,84 @@ def create_gds_scans(config):
         create_scan(create_scan_config(scan))
 
 
+def find_new_rules(config, name):
+    for new_scan in config.values():
+        if new_scan["name"] == name:
+            return new_scan
+        else:
+            return False
+
+
+def get_names(config, new_scan):
+    name_list = []
+    for new_scan in config.values():
+        name_list.append(new_scan)
+    return name_list
+
+
 def load_scan_config():
     with open("scan_config/scan.toml", "r") as f:
         return toml.load(f)
 
 
+def compare_rrules(new_scan, scan_rrules):
+    rrules = f"FREQ={new_scan['rrules.freq']};INTERVAL={new_scan['rrules.interval']};BYDAY={new_scan['rrules.byday']}"
+    if scan_rrules == rrules:
+        return True
+    else:
+        return False
+
+
+def check_remaining_rules(scan, new_scan, config):
+    rules = ["enabled", "starttime", "text_targets"]
+    for rule in rules:
+        print(scan)
+        print(new_scan)
+        print(f"Nessus: {scan['name']}: {scan[rule]}\nConfig: {new_scan['name']}: {new_scan[rule]}")
+        if scan[rule] != new_scan[rule]:
+            print(f"Nessus: {scan[rule]}\nConfig: {new_scan[rule]}")
+            return False
+        else:
+            return True
+
+
+def compare_targets(new_scan, id):
+    describe_scan(id)
+    # Check if they are the same here
+
+
+def check_scan():
+    scan_list = list_scans()
+    config = load_scan_config()
+    scans = scan_list["scans"]
+    for scan in scans:
+        new_scan = find_new_rules(config, scan["name"])
+        if scan["name"] in get_names(config, new_scan):
+            print(f"Scan {scan['name']} already exists checking for changed config.")
+            if find_new_rules(config, scan["name"]):
+                scan_rrules = scan["rrules"]
+                if compare_rrules(new_scan, scan_rrules):
+                    print(f"Scan {scan['name']} already exists with rrules given, skipping...")
+                    pass
+                elif compare_targets(new_scan, scan["id"]):
+                    print(f"Scan {scan['name']} already exists with targets given, skipping...")
+                    pass
+                elif check_remaining_rules(scan, new_scan, config):
+                    print(f"Scan {scan['name']} already exists with config given, skipping...")
+                    pass
+                else:
+                    update_gds_scans(config)
+        else:
+            print("New scan config found, creating...")
+            #Logic not working, still creating every time.
+            create_gds_scans(config)
+
+
 def main():
     print("Scheduling scans...")
-    config = load_scan_config()
-    create_gds_scans(config)
+    # config = load_scan_config()
+    # create_gds_scans(config)
+    check_scan()
 
 
 if __name__ == "__main__":
