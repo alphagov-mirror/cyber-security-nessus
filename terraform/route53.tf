3077cab2e7d8a09d260d7fdf432b8cdd932b812b
Deniz Genc <25902330+denizgenc@users.noreply.github.com>
2020-05-06 12:00:50 +0100

    Use Route53 resource instead of a data resource
    This is because the Route53 zone referred to at the top of
    `route53.tf` does not currently exist, causing an error when trying to
    run `terraform plan`.

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -1,9 +1,9 @@
-data "aws_route53_zone" "root_domain" {
+resource "aws_route53_zone" "root_domain" {
   name = "${var.root_domain}."
 }
 
 resource "aws_route53_record" "fqdn" {
-  zone_id = data.aws_route53_zone.root_domain.zone_id
+  zone_id = aws_route53_zone.root_domain.zone_id
   name    = var.fqdn
   type    = "A"
 
