2c7df5e5bc82ed79cfccc572fe923dfbecd41028
Samuel Pritchard <samuel.pritchard@digital.cabinet-office.gov.uk>
2020-05-14 11:18:55 +0100

    Add tf variables to SSM

--- terraform/data.tf
+++ terraform/data.tf
@@ -9,6 +9,15 @@ data "aws_ssm_parameter" "nessus_password" {
 data "aws_ssm_parameter" "nessus_licence" {
   name = "/nessus/licence"
 }
+
+data "aws_ssm_parameter" "nessus_base_url" {
+  name = "/nessus/public_base_url"
+}
+
+data "aws_ssm_parameter" "account_id" {
+  name = "/nessus/account_id"
+}
+
 #
 # data "aws_ssm_parameter" "nessus_access_key" {
 #   name  = "/nessus/access_key"
