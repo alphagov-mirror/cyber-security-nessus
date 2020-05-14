840881200fee6c1c566ffbc251ea593c14862a2e
Alex Kinnane <17098249+akinnane@users.noreply.github.com>
2020-05-14 15:08:12 +0100

    Terraform formatting

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -8,7 +8,7 @@ resource "aws_cloudwatch_log_group" "nessus_data" {
 }
 
 resource "aws_cloudwatch_log_subscription_filter" "log_subscription" {
-  name            = "log_subscription"
+  name = "log_subscription"
 
   log_group_name  = "/gds/nessus-scans"
   filter_pattern  = ""
