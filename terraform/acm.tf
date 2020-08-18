0da18688b77e6f2660be25ab7894397af376b7ed
Aaron Hine <aaron.hine@digital.cabinet-office.gov.uk>
2020-08-18 17:05:23 +0100

    changed aws provider version due to known bug

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -4,22 +4,15 @@ resource "aws_acm_certificate" "fqdn" {
 }
 
 resource "aws_route53_record" "subdomain_validation" {
-  for_each = {
-    for domain_validation_option in aws_acm_certificate.fqdn.domain_validation_options : domain_validation_option.domain_name => {
-      name   = domain_validation_option.resource_record_name
-      record = domain_validation_option.resource_record_value
-      type   = domain_validation_option.resource_record_type
-    }
-  }
+  name    = aws_acm_certificate.fqdn.domain_validation_options.0.resource_record_name
+  type    = aws_acm_certificate.fqdn.domain_validation_options.0.resource_record_type
+  records = [aws_acm_certificate.fqdn.domain_validation_options.0.resource_record_value]
 
-  name    = each.value.name
-  type    = each.value.type
-  records = [each.value.record]
   zone_id = data.aws_route53_zone.nessus_domain.id
   ttl     = 60
 }
 
 resource "aws_acm_certificate_validation" "subdomain_wildcard" {
   certificate_arn         = aws_acm_certificate.fqdn.arn
-  validation_record_fqdns = [for record in aws_route53_record.subdomain_validation : record.fqdn]
+  validation_record_fqdns = [aws_route53_record.subdomain_validation.fqdn]
 }
